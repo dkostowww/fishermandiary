950314366852fa3574c1b891bd90576b3501eb7b
Dimitar Kostov <dkostowww@gmail.com>
2023-01-22 18:04:28 +0200

    Update manifest and jsx tags

--- front/src/components/common/Forms/SignInForm/SignInForm.js
+++ front/src/components/common/Forms/SignInForm/SignInForm.js
@@ -3,40 +3,40 @@ import './styles.css'
 
 const SignInForm = () => {
   return (
-    <div class="sign-in-form">
-      <div class="fd-content-widget fd-login-widget white-bg col-12">
-        <header class="text-center">
-          <div class="square"></div>
+    <div className="sign-in-form">
+      <div className="fd-content-widget fd-login-widget white-bg col-12">
+        <header className="text-center">
+          <div className="square"></div>
           <h1>Fisherman Diary</h1>
         </header>
-        <form action="index.html" class="fd-login-form">
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-addon"><i class="fa fa-user fa-fw"></i></div>	        		
-              <input type="text" class="form-control" placeholder="johndoe@mail.com" />           
+        <form action="index.html" className="fd-login-form">
+          <div className="form-group">
+            <div className="input-group">
+              <div className="input-group-addon"><i className="fa fa-user fa-fw"></i></div>	        		
+              <input type="text" className="form-control" placeholder="johndoe@mail.com" />           
             </div>	
           </div>
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-addon"><i class="fa fa-key fa-fw"></i></div>	        		
-              <input type="password" class="form-control" placeholder="******" />           
+          <div className="form-group">
+            <div className="input-group">
+              <div className="input-group-addon"><i className="fa fa-key fa-fw"></i></div>	        		
+              <input type="password" className="form-control" placeholder="******" />           
             </div>	
           </div>	          	
-          <div class="form-group">
-            <div class="checkbox squaredTwo">
+          <div className="form-group">
+            <div className="checkbox squaredTwo">
               <input type="checkbox" id="c1" name="cc" />
               <label for="c1">
                 <span></span>Remember me
               </label>
             </div>				    
           </div>
-          <div class="form-group">
-            <button type="submit" class="fd-blue-button width-100">Sign In</button>
+          <div className="form-group">
+            <button type="submit" className="fd-blue-button width-100">Sign In</button>
           </div>
         </form>
       </div>
-      <div class="fd-content-widget fd-login-widget fd-register-widget white-bg">
-        <p>Not a registered user yet? <strong><a href="/" class="blue-text">Sign up now!</a></strong></p>
+      <div className="fd-content-widget fd-login-widget fd-register-widget white-bg">
+        <p>Not a registered user yet? <strong><a href="/" className="blue-text">Sign up now!</a></strong></p>
       </div>
     </div>
   )
--- front/src/components/common/Forms/SignUpForm/SignUpForm.js
+++ front/src/components/common/Forms/SignUpForm/SignUpForm.js
@@ -3,39 +3,39 @@ import './styles.css'
 
 const SignUpForm = () => {
   return (
-    <div class="sign-up-form">
-      <div class="fd-content-widget fd-login-widget white-bg col-12">
-        <header class="text-center">
-          <div class="square"></div>
+    <div className="sign-up-form">
+      <div className="fd-content-widget fd-login-widget white-bg col-12">
+        <header className="text-center">
+          <div className="square"></div>
           <h1>Fisherman Diary</h1>
         </header>
-        <form action="index.html" class="fd-login-form">
-          <div class="form-group">                  
-            <label for="inputFirstName">First Name</label>
-            <input type="text" class="form-control" id="inputFirstName" placeholder="David" />                  
+        <form action="index.html" className="fd-login-form">
+          <div className="form-group">                  
+            <label htmlFor="inputFirstName">First Name</label>
+            <input type="text" className="form-control" id="inputFirstName" placeholder="David" />                  
           </div>
-          <div class="form-group">                  
-              <label for="inputLastName">Last Name</label>
-              <input type="text" class="form-control" id="inputLastName" placeholder="Johnson" />                  
+          <div className="form-group">                  
+              <label htmlFor="inputLastName">Last Name</label>
+              <input type="text" className="form-control" id="inputLastName" placeholder="Johnson" />                  
           </div>
-          <div class="form-group">                  
-              <label for="inputUsername">Username</label>
-              <input type="text" class="form-control" id="inputUsername" placeholder="Example-user" />                  
+          <div className="form-group">                  
+              <label htmlFor="inputUsername">Username</label>
+              <input type="text" className="form-control" id="inputUsername" placeholder="Example-user" />                  
           </div>
-          <div class="form-group">                  
-              <label for="inputEmail">Email</label>
-              <input type="email" class="form-control" id="inputEmail" placeholder="example@mail.com" />                  
+          <div className="form-group">                  
+              <label htmlFor="inputEmail">Email</label>
+              <input type="email" className="form-control" id="inputEmail" placeholder="example@mail.com" />                  
           </div>
-          <div class="form-group">                  
-              <label for="inputPassword">Password</label>
-              <input type="password" class="form-control" id="inputPassword" />
+          <div className="form-group">                  
+              <label htmlFor="inputPassword">Password</label>
+              <input type="password" className="form-control" id="inputPassword" />
           </div>
-          <div class="form-group">                  
-              <label for="inputConfirmPassword">Confirm Password</label>
-              <input type="password" class="form-control" id="inputConfirmPassword" />
+          <div className="form-group">                  
+              <label htmlFor="inputConfirmPassword">Confirm Password</label>
+              <input type="password" className="form-control" id="inputConfirmPassword" />
           </div>
-          <div class="form-group">
-            <button type="submit" class="fd-blue-button width-100">Sign Up</button>
+          <div className="form-group">
+            <button type="submit" className="fd-blue-button width-100">Sign Up</button>
           </div>
         </form>
       </div>
